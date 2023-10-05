b1fbc6530d80ec4489e6608a7ccf8108a678610a
Max Levine <max@maxlevine.co.uk>
2023-10-05 23:18:18 +0100

    fix: add python type annotations

--- scraper.py
+++ scraper.py
@@ -1,9 +1,10 @@
 import argparse
+from typing import List, Tuple
 import os
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-def parse_arguments():
+def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Bass Guitar Scraper")
     parser.add_argument("urls", nargs="*", default=os.getenv("SCRAPE_URLS", "").split(";"),
                         help="URLs to scrape, separated by space")
@@ -12,19 +13,27 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
-def scrape_bass_guitar_info(urls, selector):
+def scrape_bass_guitar_info(urls: List[str], selector: str) -> Tuple[List[str], List[str]]:
+    bass_guitar_names = []
+    bass_guitar_features = []
+
     for url in urls:
         response = requests.get(url)
         if response.status_code != 200:
             print("Failed to fetch the webpage")
             continue
-        
+
         soup = BeautifulSoup(response.content, 'html.parser')
-        elements = soup.select(selector)
-        # Extract information based on the provided selector
-        for element in elements:
-            print(element.text)
+
+        # Scrape relevant information from the webpage
+        # Example: Get bass guitar names and features
+        bass_guitar_names.extend([element.text for element in soup.select(selector)])
+        bass_guitar_features.extend([element.text for element in soup.select('div.bass-guitar-features')])
+
+    return bass_guitar_names, bass_guitar_features
 
 if __name__ == "__main__":
     args = parse_arguments()
-    scrape_bass_guitar_info(args.urls, args.selector)
+    scraped_names, scraped_features = scrape_bass_guitar_info(args.urls, args.selector)
+    print("Bass Guitar Names:", scraped_names)
+    print("Bass Guitar Features:", scraped_features)
